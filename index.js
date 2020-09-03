03511b12c94cd2f068e6219a11687bfbd779b593
tec-wizard <64216681+tec-wizard@users.noreply.github.com>
2020-09-03 19:25:04 +0530

    Ep-01

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on("ready", () => {
+    console.log("Ready to go!")
+    client.user.setActivity("under development!", { type: "LISTENING"})
+})
+client.on("message", message => {
+    if(message.content === "ping") {
+        return message.channel.send("Pong! :ping_pong:")
+    }
+})
+
+client.login("NzUwMzk2MjMxMzYwMTg0MzQw.X0568w.C5CBnIYQDW5xGoY1GXvYBRTqCfo")

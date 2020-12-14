0a500b4e4de001c496650397a9663022b3de2a65
tec-wizard <64216681+tec-wizard@users.noreply.github.com>
2020-12-14 18:50:37 +0530

    Ep-09, Slowmode

---
+++ commands/utility/slowmode.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: "slowmode",
+    description: "Set the slowmode of a channel.",
+    run: async(client, message, args) => {
+        if(!message.member.hasPermission("ADMINISTRATOR")) {
+            return message.reply("You don't have enough perms to use this command!")
+        }
+        let duration = args[0]
+        if(isNaN(duration)) return message.reply("Please give the time in seconds.")
+        let reason = args.slice(1).join(" ")
+        if(!reason) return message.reply("Please specify a reason!")
+        
+        message.channel.setRateLimitPerUser(duration, reason)
+        message.reply(`Successfully set the slowmode to ${duration} seconds with Reason - ${reason}`)
+    }
+}

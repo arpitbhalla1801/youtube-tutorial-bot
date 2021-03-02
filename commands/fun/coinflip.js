d70a2d6767f34030eeddb027b4ad26d825962c68
tec-wizard <64216681+tec-wizard@users.noreply.github.com>
2021-03-02 19:31:40 +0530

    Coinflip cmd, Ep-15

---
+++ commands/fun/coinflip.js
@@ -0,0 +1,13 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+    name: "coinflip",
+    description: "flips a coin!",
+    run: async(client, message, args) => {
+        const choices= ["heads", "tails"];
+        const choice = choices[Math.floor(Math.random() * choices.length)];
+        let embed = new MessageEmbed()
+        .setTitle("Coinflip!")
+        .setDescription(`You flipped a **${choice}**!`)
+        message.channel.send(embed)
+    }
+}
